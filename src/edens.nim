c83e0bd9e0876910c0d3d9783b1aacc262edb9d0
jiro4989 <jiroron666@gmail.com>
2019-11-13 00:03:49 +0900

    コマンドラインでエンコードに使う文字を指定できるようにした

--- src/edens.nim
+++ src/edens.nim
@@ -23,35 +23,44 @@ proc readInput(files: seq[string]): string =
   else:
     files[0].readFile()
 
-proc encodeDecode(decode: bool, name: string, files: seq[string]): (string, int) =
-  let dict = parseFile(dictFile).to(seq[Dict])
-  var words: seq[string]
-  for d in dict:
-    if d.name == name:
-      words = d.words
-      break
-  if words.len < 1:
-    echo "NG"
-    return ("", 1)
+proc encodeDecode(decode: bool, words: seq[string], files: seq[string]): (string, int) =
   let content = readInput(files)
   let s =
     if decode: util.decode(content, words)
     else: util.encode(content, words)
   return (s, 0)
 
-proc main(decode = false, list = false, args: seq[string]): int =
+proc edens(decode = false, list = false, dict = "", words: seq[string] = @[],
+          args: seq[string]): int =
+  if dict != "":
+    dictFile = dict
+
   if list:
     let dict = parseFile(dictFile).to(seq[Dict])
     for d in dict:
       echo d.name
     return 0
 
-  if args.len < 1:
+  var words2: seq[string]
+  var files: seq[string]
+  if 0 < words.len:
+    words2 = words
+    files = args
+  elif 1 <= args.len:
+    let name = args[0]
+    files = args[1..^1]
+    let dict = parseFile(dictFile).to(seq[Dict])
+    for d in dict:
+      if d.name == name:
+        words2 = d.words
+        break
+    if words2.len < 1:
+      echo "NG"
+      return 1
+  else:
     return 1
 
-  let name = args[0]
-  let files = args[1..^1]
-  let (s, exitCode) = encodeDecode(decode, name, files)
+  let (s, exitCode) = encodeDecode(decode, words2, files)
   if exitCode != 0:
     return exitCode
   echo s
@@ -59,4 +68,4 @@ proc main(decode = false, list = false, args: seq[string]): int =
 when isMainModule and not defined(isTesting):
   import cligen
   clCfg.version = version
-  dispatch(main)
+  dispatch(edens)
--- tests/tedens.nim
+++ tests/tedens.nim
@@ -6,24 +6,17 @@ include edens
 dictFile = "src" / "dict.json"
 
 let testDataDir = "tests" / "testdata"
+let dictSushi = @["寿", "司"]
 
 suite "proc encodeDecode":
   setup:
     let testFile = testDataDir / "a.txt"
     let testEncodedFile = testDataDir / "a.encoded.txt"
   test "encodeDecode sushi":
-    let (r, e) = encodeDecode(false, "sushi", @[testFile])
+    let (r, e) = encodeDecode(false, dictSushi, @[testFile])
     check e == 0
     check r == "寿司司寿寿寿寿司寿司司寿寿寿司寿寿司司寿寿寿司司寿司司寿寿司寿寿寿司司寿寿司寿司寿寿寿寿司寿司寿司司司寿寿寿司司司寿寿寿寿寿寿司司寿寿寿寿寿司寿司司司寿寿寿司司司寿寿寿寿寿寿司司寿寿寿寿司寿寿司司司寿寿寿司司司寿寿寿寿寿寿司司寿寿寿寿司司寿司司司寿寿寿司司司寿寿寿寿寿寿司司寿寿寿司寿寿寿司司司寿寿寿司司司寿寿寿寿寿寿司司寿寿寿司寿司寿寿寿寿寿司寿司寿"
   test "encodeDecode -d sushi":
-    let (r, e) = encodeDecode(true, "sushi", @[testEncodedFile])
+    let (r, e) = encodeDecode(true, @["寿", "司"], @[testEncodedFile])
     check e == 0
     check r == "abcde\nあいうえお\n"
-  test "invalid name":
-    let (r, e) = encodeDecode(false, "tamtoiubvcxgfge", @[testEncodedFile])
-    check e != 0
-    check r == ""
-  test "invalid -d name":
-    let (r, e) = encodeDecode(true, "tamtoiubvcxgfge", @[testEncodedFile])
-    check e != 0
-    check r == ""
