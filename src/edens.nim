208556489402c4d722997c5f1b104b9debb689b8
jiro4989 <jiroron666@gmail.com>
2019-11-13 01:37:03 +0900

    ヘルプを加筆

--- src/edens.nim
+++ src/edens.nim
@@ -42,6 +42,13 @@ proc validateWords(words: seq[string]): bool =
 
 proc edens(decode = false, list = false, dict = "", words: seq[string] = @[],
           args: seq[string]): int =
+  ## 入力をエンコード/デコードします。argsの第一引数に辞書名、第二引数に処理対象
+  ## を指定します。第二引数を省略した場合は標準入力待ちになります。
+  ##
+  ## wordsを指定した場合はwordsを優先し、付属の辞書を使用しなくなります。
+  ## また、必須だった第一引数の辞書名が不要になり、第一引数にファイルを受け付
+  ## けるようになります。
+  ## wordsを指定した場合で第一引数を省略した場合は標準入力待ちになります。
   if dict != "":
     dictFile = dict
 
@@ -81,4 +88,11 @@ proc edens(decode = false, list = false, dict = "", words: seq[string] = @[],
 when isMainModule and not defined(isTesting):
   import cligen
   clCfg.version = version
-  dispatch(edens)
+  dispatch(edens,
+           short = {"dict":'D'},
+           help = {
+             "decode":"入力をデコードするモードに切り替えます",
+             "list":"エンコード/デコードに使用する辞書名の一覧を出力します",
+             "dict":"辞書ファイルを指定します",
+             "words":"辞書の代わりにエンコード/デコードに使用する文字を指定します",
+             })
